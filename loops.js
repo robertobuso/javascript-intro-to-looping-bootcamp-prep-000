dbbca7c8ee1e185b6410f4d4dea3b4f1784f816e
Roberto Busó-García <robertobuso@gmail.com>
2018-06-29 17:05:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,9 @@
 function forLoop (array) {
   for (var i = 1; 1<25; i++) {
-    if i === 1 {
-      
+    if (i === 1) {
+      console.log("I am 1 strange loop.")
+    } else {
+      console.log("I am ${i} strange loops.")
     }
   }
 }
