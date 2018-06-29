f33a8fe39348a30be63da92846be54082e337a8b
Roberto Busó-García <robertobuso@gmail.com>
2018-06-29 17:21:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@ function forLoop (array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
       var stringOne = "I am 1 strange loop.";
-          array = array.push (stringOne);
+          array = array.push ([stringOne]);
     } else {
       var stringMoreThanOne = "I am ${i} strange loops.";
-          array = array.push (stringMoreThanOne);
+          array = array.push ([stringMoreThanOne]);
     }
 return array;
   }
